260917bac5a59917e2865a7e4ca423852467a30e
TakeMos <kangh0423@gmail.com>
2024-05-13 15:23:30 +0900

    [FEAT] Jamie 뷰 제작

--- GitHub Practice 2024/GitHub Practice 2024/View/LetterViews/JamieView.swift
+++ GitHub Practice 2024/GitHub Practice 2024/View/LetterViews/JamieView.swift
@@ -5,4 +5,15 @@
 //  Created by kim kanghyeok on 5/13/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct JamieView: LetterView {
+    var name: String = "Jamie"
+    
+    var body: some View {
+        Text("MC2 잘 마무리 좀 해보자.")
+    }
+}
+#Preview {
+    JamieView()
+}
