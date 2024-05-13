003e670825241a9ba162550245dc68197531e1fa
Hajin Lee <springlhj2@naver.com>
2024-05-13 15:01:54 +0900

    [Feat] {피카} 뷰제작

--- GitHub Practice 2024/GitHub Practice 2024/View/LetterViews/PikaView.swift
+++ GitHub Practice 2024/GitHub Practice 2024/View/LetterViews/PikaView.swift
@@ -7,12 +7,19 @@
 
 import SwiftUI
 
-struct PikaView: View {
+struct PikaView: LetterView {
+    var name: String = "Pika"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Text("MC2를 맞이한 파카의 다짐")
+            Text("앱 출 시 하 기")
+            Text("특정 뷰 내 힘으로 구현하기!!!!!!!!")
+        }
     }
 }
 
 #Preview {
     PikaView()
 }
+
