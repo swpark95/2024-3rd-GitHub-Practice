a197cf8c239f506657e4e76eef7dd28b78359fae
Skylar <minkim0817@icloud.com>
2024-05-13 15:35:41 +0900

    [Feat]Skylar 뷰 제작

---
+++ GitHub Practice 2024/GitHub Practice 2024/View/LetterViews/SkylarView.swift
@@ -0,0 +1,33 @@
+//
+//  SkylarView.swift
+//  GitHub Practice 2024
+//
+//  Created by Kim Min(Skylar) on 5/13/24.
+//
+
+import SwiftUI
+
+struct SkylarView: View {
+    var body: some View {
+        
+        VStack {
+            Text("MC2를 맞이한 나의 다짐")
+                .font(.largeTitle)
+                .bold()
+                .padding(20)
+            
+            Text("Do My Best 하겠슴다! 🔥🫡")
+                .font(.title2)
+                .background(.purple)
+                .opacity(0.8)
+            
+        }
+        
+        
+        
+    }
+}
+
+#Preview {
+    SkylarView()
+}
