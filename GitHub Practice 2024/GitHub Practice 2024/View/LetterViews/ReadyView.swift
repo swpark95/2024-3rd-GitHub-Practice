7ba3efc9a1bfc52f8dbe477d86cb13a516506f52
Cho YeonJi <choyeonji@10-141-55-63.0200.pos.kr.iosda.org>
2024-05-13 15:11:31 +0900

    [Feat] 리스트에 레디  뷰 추가

--- GitHub Practice 2024/GitHub Practice 2024/View/LetterViews/ReadyView.swift
+++ GitHub Practice 2024/GitHub Practice 2024/View/LetterViews/ReadyView.swift
@@ -7,9 +7,12 @@
 
 import SwiftUI
 
-struct ReadyView: View {
+struct ReadyView: LetterView {
+    var name: String = "Ready"
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("MC2 레디의 다짐")
+        Text("사람들한테 물어물어 계획대로 기능 완성하기!!")
+        Text("잘 부탁드립니다 스승님들...")
     }
 }
 
