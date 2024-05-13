6e198c37a6c9bf3b117dc4b729475e1380759c6f
Cho YeonJi <choyeonji@10-141-55-63.0200.pos.kr.iosda.org>
2024-05-13 14:55:36 +0900

    [Feat] 레디 뷰제작

---
+++ GitHub Practice 2024/GitHub Practice 2024/View/LetterViews/ReadyView.swift
@@ -0,0 +1,18 @@
+//
+//  ReadyView.swift
+//  GitHub Practice 2024
+//
+//  Created by Cho YeonJi on 5/13/24.
+//
+
+import SwiftUI
+
+struct ReadyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReadyView()
+}
