805cb8ff3428b1e466f89fd928eeefa33418adcb
신혜연 <shy020916@naver.com>
2024-05-13 20:39:01 +0900

    [Feat] Happy  뷰 제작

---
+++ GitHub Practice 2024/GitHub Practice 2024/View/LetterViews/HappyView.swift
@@ -0,0 +1,18 @@
+//
+//  HappyView.swift
+//  GitHub Practice 2024
+//
+//  Created by 신혜연 on 5/13/24.
+//
+
+import SwiftUI
+
+struct HappyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HappyView()
+}
