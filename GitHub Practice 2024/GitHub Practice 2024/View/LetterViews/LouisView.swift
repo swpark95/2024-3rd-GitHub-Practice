db839584170a3f0d5354592b33718fbca76dda19
ezzkimm <dmswjd1515@gmail.com>
2024-05-13 15:22:31 +0900

    [Feat] Louis 뷰 제작

---
+++ GitHub Practice 2024/GitHub Practice 2024/View/LetterViews/LouisView.swift
@@ -0,0 +1,18 @@
+//
+//  LouisView.swift
+//  GitHub Practice 2024
+//
+//  Created by 김은정 on 5/13/24.
+//
+
+import SwiftUI
+
+struct LouisView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LouisView()
+}
