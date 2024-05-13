86766a94b9d4863331a726536bf02155477eb1e9
Hajin Lee <springlhj2@naver.com>
2024-05-13 14:59:44 +0900

    [Feat] {피카} 뷰제작

---
+++ GitHub Practice 2024/GitHub Practice 2024/View/LetterViews/PikaView.swift
@@ -0,0 +1,18 @@
+//
+//  PikaView.swift
+//  GitHub Practice 2024
+//
+//  Created by Hajin on 5/13/24.
+//
+
+import SwiftUI
+
+struct PikaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PikaView()
+}
