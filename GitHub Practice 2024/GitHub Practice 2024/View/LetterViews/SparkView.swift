70c9f14c2769252b7094acb9c463b4b001cb13a5
Park Sang Wook <swpark95@github.io>
2024-05-20 17:33:03 +0900

    [Feat] 스파크 뷰 제작

--- GitHub Practice 2024/GitHub Practice 2024/View/LetterViews/SparkView.swift
+++ GitHub Practice 2024/GitHub Practice 2024/View/LetterViews/SparkView.swift
@@ -3,37 +3,52 @@ import SwiftUI
 struct SparkView: LetterView {
     var name: String = "Park Sang Wook"
 
-let colors: [Color] = [.red, .green , .blue, .purple, .orange]
-	
+    let colors: [Color] = [.red, .green, .blue, .purple, .orange]
+    
     var body: some View {
-        VStack {
-            ZStack{
-		Image(systemName: "heart")
-			.resizable()
-			.foregroundStyle(.cyan)
-			.frame(height:250)
-
-		Text("MC2를 맞이한 나의 다짐")
-			.bold()
-			.padding(.bottom)
-	
-		}
-	HStack{
-		Text("새로운 사람들!")
-		.padding(.bottom)
-
-		ForEach(colors, id:\.description){ color in
-			Image(systemName: "person")
-				.resizable()
-				.scaletoFit()
-				.foregroundStyle(Color)
-				.frame(width:30)			
-			
-		}
-	}
+        VStack(alignment: .leading) {
+            ZStack {
+                Image(systemName: "heart")
+                    .resizable()
+                    .foregroundStyle(.cyan)
+                    .frame(height: 250)
+                
+                Text("스파크의 목표")
+                    .bold()
+                    .padding(.bottom)
+            }
+            
+            HStack {
+                Text("새로운 사람들!")
+                    .padding(.bottom)
+                
+                ForEach(colors, id: \.description) { color in
+                    Image(systemName: "person")
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundStyle(color)
+                        .frame(width: 30)
+                }
+            }
+            
+            HStack {
+                Image(systemName: "book")
+                
+                Text("즐거운 주제!")
+                
+                Image(systemName: "book")
+            }
+            
+            Text("우리 팀의 핵심 타겟인 대학원생의 문제를 해결하는 앱을 만들거에요!")
+                .padding(.bottom, 30)
+            
+            Text("스빡꾸빡꾸")
+                .font(.title)
         }
         .padding()
     }
 }
 
-
+#Preview {
+    SparkView()
+}
